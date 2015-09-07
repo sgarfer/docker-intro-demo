3a377829390f96d66614c48cf6dc4b8978e7ae43
Javier Moreno <email@javier-moreno.com>
2015-09-07 13:06:06 +0200

    First version of the webservice.

---
+++ webservice.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var server=http.createServer(function(request, response){
+       console.log('Knock knock.');
+       response.writeHead( 
+              200, {"Content-Type": "application/json"});
+       var data = { 'message' : 'Hello Sweetie!!!' };
+       response.end(JSON.stringify(data) + '\n');
+});
+
+server.listen(8888);
+console.log('I hate you.');
+
